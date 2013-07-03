69b424f25ddc121cba10cfdb90ccbd88e9671a82
Byronlee <lbyronlee@gmail.com>
2013-07-03 16:44:49 +0000

    this is my style

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ByronleeStyle::Application.config.secret_key_base = 'c213111f24f6ae72177a452b81070c410ce3c9384af621cf3084ff0587a097e78aae03dcb896c71df102451ca72482323610b3a41c6f60ae5a3f991b57a29b82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ByronleeStyle::Application.config.session_store :cookie_store, key: '_byronlee-style_session'
