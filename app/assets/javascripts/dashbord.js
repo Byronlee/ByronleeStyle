5c235188c28a778c8ce770abf35b56f9deff2283
Byronlee <lbyronlee@gmail.com>
2013-07-10 10:15:05 +0000

    使左边动起来

--- app/assets/javascripts/dashbord.js
+++ app/assets/javascripts/dashbord.js
@@ -1,5 +1,6 @@
 $(function () {
 
+   // 右边终端效果
     var html ='(● ◡ ●)ﾉ♥ 嗨，欢迎光临！我叫李江华，你想了解我的什么呢？\n用法：\n　　1.　简洁版简历\n　　2.　文艺版简历\n　　3.　github版简历\n　　4.　给我留言\n请输入你想了解的编号：\n' ;
     var jqconsole = $('#console').jqconsole(html, 'Ξ ~/ByronleeStyle git:(master) ▶ ','I like you! and you?');
 
@@ -28,5 +29,35 @@ $(function () {
           });
         };
     startPrompt();
+
+
+    // 左面黑客效果
+    var TheMatrix = {
+	height : window.screen.height,    //浏览器高
+	width : window.screen.width*0.32 ,    //浏览器宽
+	speed : 35,    //下降速度
+
+	createDIV : setInterval(function(){    //生成div
+            var div = document.createElement("div");
+            var posy = 0;
+            div.style.left = Math.round(Math.random()*TheMatrix.width) +"px";
+            div.style.fontSize = Math.round(Math.random()*50) +"px";
+            div.style.opacity = Math.random().toFixed(1);
+            div.innerHTML = Math.round(Math.random()*100000000000000000);
+            document.getElementById("heike").appendChild(div);
+            var moveDIV = setInterval(function(){
+                //alert(posy);
+                div.innerHTML =  Math.round(Math.random()*100000000000000000);
+                div.style.top =  posy - TheMatrix.height + "px";
+                posy += TheMatrix.speed;
+                if(parseInt(div.style.top) > TheMatrix.height){                
+                    document.getElementById("heike").removeChild(div);
+                    clearInterval(moveDIV);
+                }            
+            }, 20);
+	},25)
+    }
+  //x  setInterval(TheMatrix.createDIV(),50);
+
 });
   
