5f539d63c4ccb4a12b51e094fe412d1adcfe3efa
Byronlee <lbyronlee@gmail.com>
2013-10-05 22:43:03 +0000

    邮件中添加时间戳

--- app/controllers/dashbord_controller.rb
+++ app/controllers/dashbord_controller.rb
@@ -20,20 +20,21 @@ class DashbordController < ApplicationController
 
   def message
     Message.create(support_hash(request).merge!({:message => params[:comments]}))
-    send_email "简历留言通知服务","留言信息：　　"+ params[:comments]+"．　　　　　　　　　－－－－－－－－－　"+user_infor(request)
+    send_email "简历留言通知服务","留言信息："+ params[:comments]+"．　　　　　　　　　－－－－－－－－－　"+user_infor(request)
     redirect_to root_path
   end
   
   
   private
   def support_hash request
-    { :ip => request.remote_ip,
+    {  :ip => request.remote_ip,
        :user_agent => request["HTTP_USER_AGENT"],
+       :time => Time.now.to_s ,
        :ip_infor => IpReverse.reverse(request.remote_ip)
      }
   end
 
   def user_infor request
-    "用户ip信息:　　"+IpReverse.reverse(request.remote_ip).to_s+"　　浏览器操作系统信息:　　"+env["HTTP_USER_AGENT"]
+    "用户ip信息:"+IpReverse.reverse(request.remote_ip).to_s+"　　浏览器操作系统信息:"+env["HTTP_USER_AGENT"]
   end
 end
